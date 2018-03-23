d806f2774bfe18dcb946a284a27a5fda958fefa8
murielsouza <muriel-souza@live.com>
2018-03-23 14:26:18 -0300

    crud completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,19 +13,24 @@ export class AppComponent {
     codigo = null;
     nome = null;
     descricao = null;
+    data = null;
+    ativo = null;
+    tipo = null;
+    periodo = null;
+
     disciplinas = [
         new Disciplina(1001, 'Língua Portuguesa','O objetivo norteador da BNCC de Língua Portuguesa ' +
             'é garantir a todos os alunos o acesso aos saberes linguísticos necessários para a ' +
             'participação social e o exercício da cidadania, pois é por meio da língua que o ser ' +
             'humano pensa, comunica-se, tem acesso à informação, expressa e defende pontos de ' +
-            'vista, partilha ou constrói visões de mundo e produz conhecimento.'),
+            'vista, partilha ou constrói visões de mundo e produz conhecimento.','19 de março de 2018', true, 'Primário', '3º'),
         new Disciplina(1002, 'Educação Física', 'A Educação Física é o componente curricular ' +
             'que tematiza as práticas corporais em suas diversas formas de codificação e ' +
             'significação social, entendidas como manifestações das possibilidades ' +
             'expressivas dos sujeitos e patrimônio cultural da humanidade. Nessa concepção, ' +
             'o movimento humano está sempre inserido no âmbito da cultura e não se limita a ' +
             'um deslocamento espaço-temporal de um segmento corporal ou de um corpo todo. ' +
-            'Logo, as práticas corporais são textos culturais passíveis de leitura e produção.'),
+            'Logo, as práticas corporais são textos culturais passíveis de leitura e produção.','19 de março de 2018', false, 'Primário', '5º'),
         new Disciplina(1003, 'Inglês', 'Aprender a língua inglesa propicia a criação de novas ' +
             'formas de engajamento e participação dos alunos em um mundo social cada vez mais ' +
             'globalizado e plural, em que as fronteiras entre países e interesses pessoais, ' +
@@ -35,7 +40,7 @@ export class AppComponent {
             'nesse sentido, abre novos percursos de acesso, construção de conhecimentos e ' +
             'participação social. É esse caráter formativo que inscreve a aprendizagem de ' +
             'inglês em uma perspectiva de educação linguística, consciente e crítica, na ' +
-            'qual as dimensões pedagógicas e políticas são intrinsecamente ligadas.'),
+            'qual as dimensões pedagógicas e políticas são intrinsecamente ligadas.','19 de março de 2018', true, 'Primário', '5º'),
         new Disciplina(1004, 'Matemática', 'No Ensino Fundamental, essa área, por meio da ' +
             'articulação de seus diversos campos – Aritmética, Álgebra, Geometria, ' +
             'Estatística e Probabilidade – precisa garantir que os alunos relacionem ' +
@@ -46,14 +51,14 @@ export class AppComponent {
             'resolver problemas, aplicando conceitos, procedimentos e resultados para ' +
             'obter soluções e interpretá-las segundo os contextos das situações. A dedução ' +
             'de algumas propriedades e a verificação de conjecturas, a partir de outras, ' +
-            'podem ser estimuladas, sobretudo ao final do Ensino Fundamental.'),
+            'podem ser estimuladas, sobretudo ao final do Ensino Fundamental.','19 de março de 2018', true, 'Secundário', '6º'),
         new Disciplina(1005, 'Ciências','Ao estudar Ciências, as pessoas aprendem a respeito ' +
             'de si mesmas, da diversidade e dos processos de evolução e manutenção da vida, ' +
             'do mundo material – com os seus recursos naturais, suas transformações e fontes ' +
             'de energia –, do nosso planeta no Sistema Solar e no Universo e da aplicação ' +
             'dos conhecimentos científicos nas várias esferas da vida humana. ' +
             'Essas aprendizagens, entre outras, possibilitam que os alunos compreendam, ' +
-            'expliquem e intervenham no mundo em que vivem.')
+            'expliquem e intervenham no mundo em que vivem.','19 de março de 2018', false, 'Primário', '7º')
     ];
 
     salvar() {
@@ -61,13 +66,21 @@ export class AppComponent {
             this.editando.codigo = this.codigo;
             this.editando.nome = this.nome;
             this.editando.descricao = this.descricao;
+            this.editando.data = this.data;
+            this.editando.ativo = this.ativo;
+            this.editando.tipo = this.tipo;
+            this.editando.periodo = this.periodo;
         } else {
-            const d = new Disciplina(this.codigo, this.nome, this.descricao);
+            const d = new Disciplina(this.codigo, this.nome, this.descricao, this.data, this.ativo, this.tipo, this.periodo);
             this.disciplinas.push(d);
         }
         this.codigo = null;
         this.nome = null;
         this.descricao = null;
+        this.data = null;
+        this.ativo = null;
+        this.tipo = null;
+        this.periodo = null;
         this.editando = null;
     }
 
@@ -87,6 +100,10 @@ export class AppComponent {
         this.codigo = disciplina.codigo;
         this.nome = disciplina.nome;
         this.descricao = disciplina.descricao;
+        this.data = disciplina.data;
+        this.ativo = disciplina.ativo;
+        this.tipo = disciplina.tipo;
+        this.periodo = disciplina.periodo;
         this.editando = disciplina;
     }
 
@@ -94,6 +111,10 @@ export class AppComponent {
         this.codigo = null;
         this.nome = null;
         this.descricao = null;
+        this.data = null;
+        this.ativo = null;
+        this.tipo = null;
+        this.periodo = null;
         this.editando = null;
     }
 
--- src/app/disciplina.model.ts
+++ src/app/disciplina.model.ts
@@ -2,10 +2,18 @@ export class Disciplina {
     codigo: number;
     nome: string;
     descricao: string;
+    data: string;
+    ativo: boolean;
+    tipo: string;
+    periodo: string;
 
-    constructor(cod:number, nome: string, descricao?: string) {
+    constructor(cod: number, nome: string, descricao?: string, data?: string, ativo?: boolean, tipo?: string, periodo?: string) {
         this.codigo = cod;
         this.nome = nome;
         this.descricao = descricao;
+        this.data = data;
+        this.ativo = ativo;
+        this.tipo = tipo;
+        this.periodo = periodo;
     }
 }
